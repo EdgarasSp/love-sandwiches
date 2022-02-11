633e13dc03a37d5fc7bd6199016196fdbd6d0e29
EdgarasSp <dev.applications.esp@gmail.com>
2022-02-11 15:35:34 +0000

    Add user request loop to repeat request fro data until provided is valid

--- run.py
+++ run.py
@@ -34,29 +34,45 @@ print(data)    """
 def get_sales_data():
     """
     Get sales figures input from the user.
+    
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    #Infinity loope until valid data is entered
+    while True:
+    
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: ")
 
-    # to test entered data = print(f"The data provided is {data_str}")"
+        # to test entered data 
+        
+        print(f"DELETEME = The data provided is {data_str}")
 
-    # split string by comma delimiter, commas are removed
-    sales_data = data_str.split(",")
+        # split string by comma delimiter, commas are removed
+        sales_data = data_str.split(",")
 
-    # this calls new function to check/validate numbers and we pass sales_data variable
-    validate_data(sales_data)
+         # to check print of entered values in this function = 
+        print(f"DELETEME = Print from get_sales_data function: {sales_data}")
 
-    # to check print of entered values in this function = 
-    print(f"Print from get_sales_data function: {sales_data}")
+        # this calls new function to check/validate numbers and we pass sales_data variable
+        # if this function is TRUE
+        if validate_data(sales_data):
+            # tel user all is ok
+            print('Data is Valid')
+            #stop this while loop
+            break
+
+       
+
+    # returns from lopp final data captured
+    return sales_data
 
 
 def validate_data(values):
     
     # this now prints entered values from not from first but second function 
-    print(f"Print from validate_data function: {values}")
+    print(f"DELETEME = Print from validate_data function: {values}")
 
     """
     Inside the try, converts all string values into integers.
@@ -64,6 +80,10 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
+        #Task 1
+        #FOR a "value" IN the "values" list, int(value) convert that value to a number
+        [int(value) for value in values]
+        #Task 2
         # if len("instancies") in variable "Values" !=(does not equil) to 6
         if len(values) != 6:
             #raise(create error message "valueerror" used for number check if TRUE)
@@ -71,11 +91,19 @@ def validate_data(values):
                 #print text and output how many instancies you actualy have in variable "values"
                 f"Exactly 6 values required, you provided {len(values)}"
             )
-            # if other non numeric error also TRUE
+            # if Task 1 (error i.e text entered so cant be converted) or Task 2 (TRY) is TRUE, run except
             # show(valueerror code) bit assign it to E variable
     except ValueError as e:
         #print text and show what was the error as E, \n show in a new line
         print(f"Invalid data: {e}, please try again.\n")
-        
-get_sales_data()
+        # returns false if code had an error and tells to continue the get_sales_data loop
+        return False
+
+    # returns true if no errors in the code, and tells to stop the get_sales_data loop
+    return True
+
+# assign the final results to data variable
+data = get_sales_data()
+
+print(F'DELETEME = Result from get_sales data function after validation etc...: {data}')
 
